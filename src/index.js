2a6bd776c903b947b7876cc289d0235e6de31af2
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-01-03 11:13:01 -0300

    Materialize css included and first layout components Footer and NavBar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import './App.css'
+//import $ from 'jquery';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
